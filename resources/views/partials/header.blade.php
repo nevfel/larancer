600b676a48840acad988532a1b1507ad5b085764
Nevfel Ermumcu <nevfelermumcu@gmail.com>
2018-04-10 22:50:39 +0300

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<div class="page-header-inner">
+    <div class="page-header-inner">
+        <div class="navbar-header">
+            <a href="{{ url('/') }}"
+               class="navbar-brand">
+                Hayat Bilişim
+            </a>
+        </div>
+        <a href="javascript:;"
+           class="menu-toggler responsive-toggler"
+           data-toggle="collapse"
+           data-target=".navbar-collapse">
+        </a>
+
+        <div class="top-menu">
+            <ul class="nav navbar-nav pull-right">
+                <li>
+                    <a href="/">Powered by Hayat Bilişim</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
